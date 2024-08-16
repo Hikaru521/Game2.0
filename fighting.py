40853ccadc2e0b223f53110e54d229cfec767d94
peter.bacsi <bacsi.peter2006@gmail.com>
2024-08-16 10:23:29 +0200

    Boss system

--- fighting.py
+++ fighting.py
@@ -6,7 +6,7 @@ import time
 def clear_screen():
     os.system('cls')
 
-mobs_list = ["Sárkány",  "Anyád", "Apád", "Anyós", "Kutya", "Zombie", "Medve", "Orbán"]
+mobs_list = ["Sárkány", "Anyád", "Apád", "Anyós", "Kutya", "Zombie", "Medve"]
 class Mobs:
     def __init__(self, hp, dmg):
         self.hp = hp
@@ -19,8 +19,19 @@ mobs = {"Sárkány": Mobs(100, random.randrange(25, 50)),
         "Kutya": Mobs(250, random.randrange(25, 50)),
         "Zombie": Mobs(200, random.randrange(25, 50)),
         "Medve": Mobs(250, random.randrange(25, 50)),
-        "Orbán": Mobs(230, random.randrange(25, 50))
         }
+
+boss_list = ["Ramatra", "Reyna"]
+class Boss:
+    def __init__(self, hp, dmg):
+        self.hp = hp
+        self.dmg = dmg
+
+
+boss = {"Ramatra": Boss(1000, random.randrange(250, 500)),
+        "Reyna": Boss(1500, random.randrange(250, 500)),
+        }
+
 class Player:
     def __init__(self, hp ,dmg, kepesseg_1, kepesseg_2, money, poti):
         self.hp = hp
@@ -32,8 +43,8 @@ class Player:
         self.damage_end = player_damage_end
         self.poti = poti
 
-player_damage_start = 200
-player_damage_end = 300
+player_damage_start = 2000
+player_damage_end = 3000
 
 player = Player(100, random.randrange(player_damage_start, player_damage_end), 1, 1, 50000000, 1)
 
@@ -47,17 +58,22 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 
 def fighting():
     os.system('cls')
-    if len(mobs_list) == 0:
-        print("Megöltél minden ellenfelet!")
-        time.sleep(2)
-        clear_screen()
-        return
-    choice = random.choice(mobs_list)
     back = 1
     while True:
-        enemy = [["Ellenfeled", "Életereje", "Életerőd"],
-                  [choice, mobs[choice].hp, player.hp]]
-        print(tabulate(enemy, headers='firstrow', tablefmt='grid'))
+        if len(mobs_list) > 0:
+            choice = random.choice(mobs_list)
+            choice_m_or_b = 0
+        elif len(mobs_list) == 0:
+            choice = random.choice(boss_list)
+            choice_m_or_b = 1
+        if choice_m_or_b == 0:
+            enemy = [["Ellenfeled", "Életereje", "Életerőd"],
+                    [choice, mobs[choice].hp, player.hp]]
+            print(tabulate(enemy, headers='firstrow', tablefmt='grid'))
+        elif choice_m_or_b == 1:
+            enemy = [["Ellenfeled", "Életereje", "Életerőd"],
+                    [choice, boss[choice].hp, player.hp]]
+            print(tabulate(enemy, headers='firstrow', tablefmt='grid'))
         lehetosegek = [["Sima támadáshoz", "Képesség használatához", "Healeléshez", ],
                        ["0", "1", "+"]]
         print(tabulate(lehetosegek, headers='firstrow', tablefmt='grid'))
@@ -65,19 +81,34 @@ def fighting():
         player_turn = True
         if player_turn:
             if console == "0":
-                mobs[choice].hp -= player.dmg
-                print(f"Damageltél a mobra {player.dmg}-t!")
-                for i in range(8):
-                    printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
-                    time.sleep(0.25)
-                player_turn = False
-                clear_screen()
-                if mobs[choice].hp <= 0:
-                    mobs_list.remove(choice)
-                    print("A mob meghalt!")
-                    time.sleep(2)
+                if choice_m_or_b == 0:
+                    mobs[choice].hp -= player.dmg
+                    print(f"Damageltél a mobra {player.dmg}-t!")
+                    for i in range(8):
+                        printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
+                        time.sleep(0.25)
+                    player_turn = False
                     clear_screen()
-                    break
+                    if mobs[choice].hp <= 0:
+                        mobs_list.remove(choice)
+                        print("A mob meghalt!")
+                        time.sleep(2)
+                        clear_screen()
+                        break
+                if choice_m_or_b == 1:
+                    boss[choice].hp -= player.dmg
+                    print(f"Damageltél a bossra {player.dmg}-t!")
+                    for i in range(8):
+                        printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
+                        time.sleep(0.25)
+                    player_turn = False
+                    clear_screen()
+                    if boss[choice].hp <= 0:
+                        boss_list.remove(choice)
+                        print("A boss meghalt!")
+                        time.sleep(2)
+                        clear_screen()
+                        break
             elif console == "+":
                 if player.poti == 1:
                     player.hp = player.hp + 50
@@ -101,17 +132,30 @@ def fighting():
                 console = input("Console >")
                 if console == "1":
                     if player.kepesseg_1 == 1:
-                        damage = random.randrange(player_damage_start + 20, player_damage_end + 20)
-                        mobs[choice].hp -= damage
-                        print(f"Damageltél a mobra az Ezüstszelidítés képesség használatával {player.dmg}-t!")
+                        if choice_m_or_b == 0:
+                            damage = random.randrange(player_damage_start + 20, player_damage_end + 20)
+                            mobs[choice].hp -= damage
+                            print(f"Damageltél a mobra az Ezüstszelidítés képesség használatával {player.dmg}-t!")
 
-                        for i in range(8):
-                            printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
-                            time.sleep(0.25)
+                            for i in range(8):
+                                printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
+                                time.sleep(0.25)
 
-                        player.kepesseg_1 -= 1
-                        player_turn = False
-                        clear_screen()
+                            player.kepesseg_1 -= 1
+                            player_turn = False
+                            clear_screen()
+                        if choice_m_or_b == 1:
+                            damage = random.randrange(player_damage_start + 20, player_damage_end + 20)
+                            boss[choice].hp -= damage
+                            print(f"Damageltél a bossra az Ezüstszelidítés képesség használatával {player.dmg}-t!")
+
+                            for i in range(8):
+                                printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
+                                time.sleep(0.25)
+
+                            player.kepesseg_1 -= 1
+                            player_turn = False
+                            clear_screen()
                     elif player.kepesseg_1 == 0:
                         print("Sajnos ez a képességed elfogyott!")
                         time.sleep(1.5)
@@ -161,18 +205,32 @@ def fighting():
                 clear_screen()
 
         if player_turn == False:
-            player.hp -= mobs[choice].dmg
-            print(f"Az ellenség damagelt rád {mobs[choice].dmg}-t")
-            for i in range(8):
-                printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
-                time.sleep(0.25)
-            clear_screen()
-            player_turn = True
-            if player.hp <= 0:
-                print("Meghaltál! A játéknak vége!")
-                time.sleep(2)
+            if choice_m_or_b == 0:
+                player.hp -= mobs[choice].dmg
+                print(f"Az ellenség damagelt rád {mobs[choice].dmg}-t")
+                for i in range(8):
+                    printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
+                    time.sleep(0.25)
                 clear_screen()
-                exit()
+                player_turn = True
+                if player.hp <= 0:
+                    print("Meghaltál! A játéknak vége!")
+                    time.sleep(2)
+                    clear_screen()
+                    exit()
+            if choice_m_or_b == 1:
+                player.hp -= boss[choice].dmg
+                print(f"Az ellenség damagelt rád {boss[choice].dmg}-t")
+                for i in range(8):
+                    printProgressBar(i, 8, prefix='Progress:', suffix='Complete', length=50)
+                    time.sleep(0.25)
+                clear_screen()
+                player_turn = True
+                if player.hp <= 0:
+                    print("Meghaltál! A játéknak vége!")
+                    time.sleep(2)
+                    clear_screen()
+                    exit()
 
 
 
