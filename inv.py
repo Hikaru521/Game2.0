cba39df2117e2c613f96e6eb8b227cdf9906ed65
peter.bacsi <bacsi.peter2006@gmail.com>
2024-08-16 09:39:32 +0200

    Inventory layout update

--- inv.py
+++ inv.py
@@ -7,9 +7,15 @@ def clear_screen():
 
 def inventory():
     clear_screen()
-    inv = [["Életerőd", "Sebzésed", "1-es képesség felhasználhazóság", "2-es képesség felhasználhazóság", "Potijaid száma", "Pénzed"],
-                   [player.hp, f"{player.damage_start} - {player.damage_end}", player.kepesseg_1, player.kepesseg_2, player.poti, f"{player.money} Lumin"]]
+    inv = [["Életerőd", "Sebzésed", "Potijaid száma"],
+                   [player.hp, f"{player.damage_start} - {player.damage_end}", player.poti,]]
     print(tabulate(inv, headers='firstrow', tablefmt='grid'))
+    inv_abilites = [["1-es képesség felhasználhatóság", "2-es képesség felhasználhatóság"],
+                   [player.kepesseg_1, player.kepesseg_2,]]
+    print(tabulate(inv_abilites, headers='firstrow', tablefmt='grid'))
+    inv_money = [["Pénzed"],
+                   [f"{player.money} Lumin"]]
+    print(tabulate(inv_money, headers='firstrow', tablefmt='grid'))
     console = input("Console >")
     while True:
         if console == "exit":
